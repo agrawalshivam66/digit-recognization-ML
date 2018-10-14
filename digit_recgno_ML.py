9568dbd5977403caf5d1b2214cbcbf7141c625af
Shivam Agrawal <agrawalshivam66@gmail.com>
2018-10-15 00:03:43 +0530

    accuracy updated

--- digit_recgno_ML.py
+++ digit_recgno_ML.py
@@ -12,6 +12,7 @@ import pandas as pd
 data_train = pd.read_csv('train.csv')
 data_test = pd.read_csv('test.csv')
 
+
 x = data_train.iloc[:, 1:].values
 y = data_train.iloc[:, 0].values
 
@@ -22,25 +23,24 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 
-#  AdaBoostClassifier accuracy 0.979
+#  AdaBoostClassifier accuracy 0.738
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 clf = AdaBoostClassifier()
 clf.fit(X_train, y_train) 
 print(clf.score(X_test, y_test))
 
-# RandomForestClassifier accuracy 0.9779
+# RandomForestClassifier accuracy 0.941
 clf = RandomForestClassifier()
 clf.fit(X_train, y_train) 
 print(clf.score(X_test, y_test))
 
-# Decision tree accuracy 0.963 percent
+# Decision tree accuracy 0.844 percent
 from sklearn import tree
 clf = tree.DecisionTreeClassifier(min_samples_split=40)
 clf = clf.fit(X_train, y_train)
 print(clf.score(X_test, y_test))
 
-# Decision tree accuracy 0.965 percent
-from sklearn import tree
-clf = tree.DecisionTreeClassifier(min_samples_split=40)
-clf = clf.fit(X_train, y_train)
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C=0.0001)
+clf.fit(X_train, y_train)
 print(clf.score(X_test, y_test))
